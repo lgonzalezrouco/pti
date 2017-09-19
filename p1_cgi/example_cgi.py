3d77dcef8ad86c43bcb79a08d364fafb93c10e77
Ruben Tous <ruben.tous@adsmurai.com>
2017-09-19 10:50:35 +0200

    nc

--- p1_cgi/example_cgi.py
+++ p1_cgi/example_cgi.py
@@ -7,3 +7,5 @@ import cgi, os, re, sys, string, time
 print "Content-type: text/html\n\n"
 print "Hello world!!\n\n"
 form = cgi.FieldStorage()
+value = form.getvalue("NOM", "")
+print "<p> Form field NOM = %s</p>"%(value)
